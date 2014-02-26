96755f0939de35bbc79ef152699592ebea8eb019
ah <andreas.hruska@edaktik.at>
2014-02-26 14:05:12 +0100

    update fileheaders for publication

--- db/messages.php
+++ db/messages.php
@@ -1,4 +1,19 @@
 <?php
+// This file is part of mod_checkmark for Moodle - http://moodle.org/
+//
+// It is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// It is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// If not, see <http://www.gnu.org/licenses/>.
+
 /**
  * db/messages.php
  *
@@ -12,15 +27,14 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$messageproviders = array (
-
 /*
  * Notifications for graded submissions (teacher -> user) and
  * new submissions (user -> teacher)!
  *
  * @todo should we split implement these in two sepparate message providers?
  */
+ 
+$messageproviders = array (
     'checkmark_updates' => array (
-)
-
+	)
 );
