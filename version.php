738025f7e2ac7d687fb7f2e9235d3f0e3c98772b
Philipp Hager <office@phager.at>
2016-01-12 17:27:37 +0100

    bump version number to 2016011200 v2.9

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015122100;
+$plugin->version   = 2016011200;
 $plugin->release   = "v2.9";             // User-friendly version number!
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2015051100;         // Requires this Moodle version!
