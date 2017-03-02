ea026a54c01d353a5f62c3a16b0df008d4137b1b
Philipp Hager <office@phager.at>
2017-03-02 20:18:59 +0100

    bump version number to 2016120501 (Release v3.2.1) and update version.php

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016120500;
+$plugin->version   = 2016120501;
 $plugin->requires  = 2016120500;
 $plugin->component = 'mod_checkmark';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "v3.2.0";
+$plugin->release   = "v3.2.1";
 
