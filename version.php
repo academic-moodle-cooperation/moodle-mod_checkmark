7a1edefecb566f71ca5f8a5b79ecf881732d8c02
Philipp Hager <office@phager.at>
2018-05-02 16:59:26 +0200

    bump version number to 2018012701 release v3.4.1 and update CHANGELOG

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018012700;
+$plugin->version   = 2018012701;
 $plugin->requires  = 2017111300;
 $plugin->component = 'mod_checkmark';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "v3.4.0";
+$plugin->release   = "v3.4.1";
 
