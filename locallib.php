19f6cce4c99999e9d3b8d8ac8539ed89f561b4ea
Daniel <daniel.binder@univie.ac.at>
2019-10-07 09:34:23 +0200

    #6148 Fix codechecker issuess

--- locallib.php
+++ locallib.php
@@ -1429,7 +1429,7 @@ class checkmark {
                         }
                     }
                 }
-                $this->update_submission($submission,true);
+                $this->update_submission($submission, true);
                 if ($this->process_feedback()) {
                     $this->display_submissions(get_string('changessaved'));
                 } else {
@@ -1804,7 +1804,7 @@ class checkmark {
                         }
                     }
                 }
-                $this->update_submission($submission,true);
+                $this->update_submission($submission, true);
                 $this->display_submission($userid);
                 break;
 
@@ -2089,16 +2089,14 @@ class checkmark {
      * update_submission($submission) - updates the submission for the actual user
      *
      * @param object $submission Submission object to update
-     * @param bool $is_overwrite Indicates of submission is updated due to overwrite -> Submission date doesn't get changed
+     * @param bool $isOverwrite Indicates of submission is updated due to overwrite -> Submission date doesn't get changed
      * @throws dml_exception
      */
-    public function update_submission($submission,$is_overwrite = false) {
-        global $USER, $DB;
+    public function update_submission($submission, $isOverwrite = false) {
+        global $DB;
         $update = new stdClass();
         $update->id           = $submission->id;
-        if ($is_overwrite && isset($submission)) {
-
-        } else {
+        if (!$isOverwrite) {
             $update->timemodified = time();
             $DB->update_record('checkmark_submissions', $update);
         }
