cc245c9e826e85d7bf12df3e1dc73e998499edf5
Daniel <daniel.binder@univie.ac.at>
2020-01-31 11:53:37 +0100

    #6497 Fix more CC issues

--- classes/grades/gradeitems.php
+++ classes/grades/gradeitems.php
@@ -23,7 +23,7 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 namespace mod_checkmark\grades;
-
+defined('MOODLE_INTERNAL') || die();
 use \core_grades\local\gradeitem\itemnumber_mapping;
 
 /**
