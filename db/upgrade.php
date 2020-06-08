3a12a18c0bdc91ed6feb86eae29691bb38753c27
Daniel <daniel.binder@univie.ac.at>
2020-06-08 19:09:29 +0200

    #6550 Update version.php, Readme.md and upgrade.php

--- db/upgrade.php
+++ db/upgrade.php
@@ -1114,7 +1114,7 @@ function xmldb_checkmark_upgrade($oldversion) {
         upgrade_mod_savepoint(true, 2019012000, 'checkmark');
     }
     // Add columns to table checkmark_overrides for enabling dynamic group overrides.
-    if ($oldversion < 2020052001) {
+    if ($oldversion < 2020060800) {
 
         // Define key userid (foreign) to be dropped form checkmark_overrides.
         $table = new xmldb_table('checkmark_overrides');
