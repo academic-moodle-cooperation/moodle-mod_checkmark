3306143fe7a55fc623b2901bc450fea50b80c57c
Thomas Wedekind <thomas.wedekind@univie.ac.at>
2021-06-10 11:12:44 +0200

    Update version.php

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021052800;
+$plugin->version   = 2021052801;
 $plugin->requires  = 2021051700;
 $plugin->component = 'mod_checkmark';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "v3.11.0";
+$plugin->release   = "v3.11.1";
 
