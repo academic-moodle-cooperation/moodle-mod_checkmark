1ce8cd0da1d017e073957774ed03222c5df2bf8c
Daniel <daniel.binder@univie.ac.at>
2021-10-25 18:49:42 +0200

    #6990 Fix new codechecker errors

--- classes/local/exporttemplates/grades.php
+++ classes/local/exporttemplates/grades.php
@@ -45,7 +45,7 @@ class grades extends basetemplate {
      * @param \checkmark|int $checkmarkorcmid checkmark object or course module id of checkmark instance
      * @return grades
      */
-    static public function get_table_instance($uniqueid, $checkmarkorcmid = null) {
+    public static function get_table_instance($uniqueid, $checkmarkorcmid = null) {
         return new grades($uniqueid, $checkmarkorcmid);
     }
 
