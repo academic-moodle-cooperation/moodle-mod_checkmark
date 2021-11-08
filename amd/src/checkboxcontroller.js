16ff2091435f6b0a43b3922543d34f2384be0967
Daniel <daniel.binder@univie.ac.at>
2021-11-08 20:38:13 +0100

    Fix js errors

--- amd/src/checkboxcontroller.js
+++ amd/src/checkboxcontroller.js
@@ -16,7 +16,7 @@
 /**
  * checkboxcontroller.js
  *
- * @package   mod_checkmark
+ * @module   mod_checkmark/checkboxcontroller
  * @author    Philipp Hager
  * @copyright 2014 Academic Moodle Cooperation {@link http://www.academic-moodle-cooperation.org}
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
