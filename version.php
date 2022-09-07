81b065b7a3f63dd69c1213167564240b9702996e
Daniel Binder <daniel.binder@univie.ac.at>
2022-09-07 18:22:43 +0200

    #7231 Update version and changelog

--- version.php
+++ version.php
@@ -29,5 +29,5 @@ $plugin->version   = 2021052806;
 $plugin->requires  = 2021051700;
 $plugin->component = 'mod_checkmark';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "v4.0.0";
+$plugin->release   = "v4.0.6";
 
