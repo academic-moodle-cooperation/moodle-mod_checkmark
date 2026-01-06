1e3c675419b7c2cc20fc40befdadb9d1b3060a71
Clemens Marx <clemens.marx@gmail.com>
2026-01-06 22:45:58 +0200

    #8459 Moodle 5.1 compatible version

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025052200;
-$plugin->requires  = 2025041400;
+$plugin->version   = 2026010600;
+$plugin->requires  = 2025100600;
 $plugin->component = 'mod_checkmark';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "v5.0.0";
+$plugin->release   = "v5.1.0";
